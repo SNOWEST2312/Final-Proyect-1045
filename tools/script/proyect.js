88fbd484ce42f149355343e429c166f8565f9b42
SNOWEST2312 <juanmimahelono@gmail.com>
2026-03-26 10:08:18 -0700

    Actualiza el js

--- tools/script/proyect.js
+++ tools/script/proyect.js
@@ -58,3 +58,70 @@ window.addEventListener('load', () => {
 //     }
 //   }
 // }
+
+// checkers board as a 2D array without loops
+
+let board = [
+  [
+    null,
+    new Pieces(0, 1, 'red', false),
+    null,
+    new Pieces(0, 3, 'red', false),
+    null,
+    new Pieces(0, 5, 'red', false),
+    null,
+    new Pieces(0, 7, 'red', false),
+  ],
+  [
+    new Pieces(1, 0, 'red', false),
+    null,
+    new Pieces(1, 2, 'red', false),
+    null,
+    new Pieces(1, 4, 'red', false),
+    null,
+    new Pieces(1, 6, 'red', false),
+    null,
+  ],
+  [
+    null,
+    new Pieces(2, 1, 'red', false),
+    null,
+    new Pieces(2, 3, 'red', false),
+    null,
+    new Pieces(2, 5, 'red', false),
+    null,
+    new Pieces(2, 7, 'red', false),
+  ],
+  [null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null],
+  [
+    new Pieces(5, 0, 'gray', false),
+    null,
+    new Pieces(5, 2, 'gray', false),
+    null,
+    new Pieces(5, 4, 'gray', false),
+    null,
+    new Pieces(5, 6, 'gray', false),
+    null,
+  ],
+  [
+    null,
+    new Pieces(6, 1, 'gray', false),
+    null,
+    new Pieces(6, 3, 'gray', false),
+    null,
+    new Pieces(6, 5, 'gray', false),
+    null,
+    new Pieces(6, 7, 'gray', false),
+  ],
+  [
+    new Pieces(7, 0, 'gray', false),
+    null,
+    new Pieces(7, 2, 'gray', false),
+    null,
+    new Pieces(7, 4, 'gray', false),
+    null,
+    new Pieces(7, 6, 'gray', false),
+    null,
+  ],
+];
